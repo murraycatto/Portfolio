b7f11b1355168a28f9e06fb2e91f8c43defcc33b
Murray Catto <murraycatto@live.co.za>
2018-01-16 20:25:04 +0200

    add new and create action for portfolio items

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -1,7 +1,33 @@
 class PortfolioItemsController < ApplicationController
 
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = PortfolioItem.all
   end
 
+  def new
+    @portfolio_item = PortfolioItem.new
+  end
+
+  def create
+    @portfolio_item = PortfolioItem.new(params.require(:portfolio_item).permit(:title,:subtitle,:body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html {redirect_to portfolio_items_path ,notice: "Your portfolio item has been created"}
+      else
+        format.html { render :new}
+      end
+    end
+  end
+
+  def show
+
+  end
+
+  private
+    def set_portfolio_item
+      @portfolio_item = PortfolioItem.find(params[:id])
+    end
 end
