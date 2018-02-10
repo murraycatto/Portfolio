6d91afa34317525366564aededd50785411e1cdb
Murray Catto <murraycatto@live.co.za>
2018-02-10 14:55:31 +0200

    create assostiated model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,8 @@
 class Blog < ApplicationRecord
   enum status: {draft:0, published:1}
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  # validations
   validates_presence_of :title, :body
 end
