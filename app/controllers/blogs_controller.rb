ff562732dec568f71232e3de9454cb472c6c8dd5
Murray Catto <murraycatto@live.co.za>
2018-02-12 21:16:41 +0200

    using diffrent layouts for diffrent controllers

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,30 +1,26 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :toggel_status, :destroy]
-
-  # GET /blogs
-  # GET /blogs.json
+  layout "blog"
+  
   def index
     @blogs = Blog.order(:title)
+    @page_title = "My Portfolio Blog"
   end
 
-  # GET /blogs/1
-  # GET /blogs/1.json
   def show
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
-  # GET /blogs/new
   def new
     @blog = Blog.new
   end
 
-  # GET /blogs/1/edit
   def edit
   end
 
-  # POST /blogs
   def create
     @blog = Blog.new(blog_params)
-
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
@@ -53,7 +49,6 @@ class BlogsController < ApplicationController
     redirect_to blogs_url, notice: 'Blog status updated.'
   end
 
-  # DELETE /blogs/1
   def destroy
     @blog.destroy
     respond_to do |format|
@@ -62,12 +57,10 @@ class BlogsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.friendly.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
