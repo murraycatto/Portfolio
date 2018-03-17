7f20324017039659106e5d642e34d14e07a4af5a
Murray Catto <murraycatto@live.co.za>
2018-03-17 10:44:43 +0200

    create a heredoc for our nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,33 @@ module ApplicationHelper
        content_tag(:p, greeting, class: "source-greeting")
      end
    end
+
+   
+
+   def nav_helper style, tag_type
+     nav_links = <<~NAV
+      <#{tag_type}>
+        <a href="#{root_path}" class="#{style} #{active? root_path}">Home</a>
+      </#{tag_type}>
+      <#{tag_type}>
+        <a href="#{about_path}" class="#{style} #{active? about_path}">About</a>
+      </#{tag_type}>
+      <#{tag_type}>
+        <a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a>
+      </#{tag_type}>
+      <#{tag_type}>
+        <a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a>
+      </#{tag_type}>
+      <#{tag_type}>
+        <a href="#{portfolio_items_path}" class="#{style} #{active? portfolio_items_path}">Portfolio</a>
+      </#{tag_type}>
+     NAV
+     # This is called a HEREDOC
+     nav_links.html_safe
+   end
+
+   def active? path
+     "active" if current_page? path
+   end
+
 end
